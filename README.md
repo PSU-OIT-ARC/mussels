d9dbeb2e439f8fb6091c92a4bfc1d8a9d3621300
Matt Johnson <mdj2@pdx.edu>
2013-06-06 01:22:27 +0200

    Django setup

---
+++ README.md
@@ -0,0 +1,17 @@
+# Install
+
+    virtualenv-2.6 --no-site-packages .env
+    source .env/bin/activate
+    pip install -r requirements.txt
+
+If you run into problems installing psycopg make sure you can execute the
+`pg_config` command. If not, you need to add the directory containing pg_config
+to your path. You can find the path with `locate pg_config`. On my machine, I
+had to do:
+
+    export PATH=/usr/pgsql-9.2/bin:$PATH
+
+# Configure
+
+    cp mussels/example_settings.py mussels/local_settings.py
+    vim mussels/local_settings.py # adjust settings
---
+++ mussels/example_settings.py
@@ -0,0 +1,24 @@
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    # ('Your Name', 'your_email@example.com'),
+)
+
+MANAGERS = ADMINS
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
+    }
+}
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = ''
+
