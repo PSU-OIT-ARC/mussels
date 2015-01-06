ecd8ccc258acb3f67a00bf2b51351202a3b71ee9
Matt Johnson <mdj2@pdx.edu>
2015-01-06 08:44:23 -0800

    Move project static files inside the Django project dir

--- mussels/settings.py
+++ mussels/settings.py
@@ -64,33 +64,18 @@ MEDIA_URL = variable("MEDIA_URL", '')
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = variable("STATIC_ROOT", 'static')
+STATIC_ROOT = os.path.join(HOME_DIR, "static")
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = variable("STATIC_URL",'/static/')
+STATIC_URL = "/static"
 
 # Additional locations of static files
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(HOME_DIR, "static"),
-)
-
-# List of finder classes that know how to find static files in
-# various locations.
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
-)
-
-# List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
+    os.path.join(PROJECT_DIR, "static"),
 )
 
 MIDDLEWARE_CLASSES = (
